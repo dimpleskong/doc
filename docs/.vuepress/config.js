158aef1679e4a3e7ead738957ead99d4b49fcc2c
dimples <happykonglf@163.com>
2022-03-09 18:45:41 +0800

    feat: 添加发布脚本

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,12 @@
 const sidebar = require('./sidebar')
 module.exports = {
-	title: 'Dimples - 文档站', // 不要轻易改，这个影响所有评论和issue的关联关系
-  base: '/toh/dimples_doc/',
-  dest: 'public',
+	title: 'Dimples - 文档站',
+  base: '/doc/',
   themeConfig: {
 		smoothScroll: true,
-		repo: 'https://gitlab.dxy.net/f2e/toh/dimples_doc',
+		repo: 'https://github.com/dimpleskong/doc',
 		repoLabel: '查看源码',
-		docsRepo: 'https://gitlab.dxy.net/f2e/toh/dimples_doc',
+		docsRepo: 'https://github.com/dimpleskong/doc',
 		docsBranch: 'master',
 		editLinks: true,
 		editLinkText: '编辑此页',
